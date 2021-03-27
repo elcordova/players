cb1bb8d76791b185c4ad1b4652e3c65dc4c3453a
elcordova <erick.lenin.03@gmail.com>
2021-03-26 23:21:03 -0500

    team-table component added

---
+++ src/app/team-table/team-table.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { defaultIfEmpty, filter, map, take } from 'rxjs/operators';
+import { Country } from '../interfaces/player';
+import { Team } from '../interfaces/team';
+import { TeamService, TeamsTableHeaders } from '../services/team.service';
+
+@Component({
+  selector: 'app-team-table',
+  templateUrl: './team-table.component.html',
+  styleUrls: ['./team-table.component.scss']
+})
+export class TeamTableComponent implements OnInit {
+  teams$!: Observable<Team[]>;
+  headers = TeamsTableHeaders
+  constructor(private teamService: TeamService) { }
+
+  ngOnInit(): void {
+    this.teamService.getTeams().pipe(take(1), defaultIfEmpty([{
+      name: 'My amazing team',
+      country: Country.Ecuador,
+      players: null!
+    }] as Team[])).subscribe(data=>{
+      console.log(data);
+    });
+
+    this.teams$ = this.teamService.getTeams()
+    .pipe(
+      take(1),
+      filter(teamArray =>!!teamArray.length),
+      defaultIfEmpty([{
+        name: 'My amazing team',
+        country: Country.Ecuador,
+        players: null!
+      }] as Team[])
+    );
+  }
+
+}
